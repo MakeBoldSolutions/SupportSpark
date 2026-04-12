d8982269b8a580132aaa13cfca0d8aedb65f397c
Mark Hazleton <mark@markhazleton.com>
2026-04-12 09:16:46 -0500

    chore: update dependencies and devDependencies in package.json; modify tsconfig.json paths

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import js from "@eslint/js";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsparser from "@typescript-eslint/parser";
-import reactPlugin from "eslint-plugin-react";
 import reactHooksPlugin from "eslint-plugin-react-hooks";
 import prettierConfig from "eslint-config-prettier";
 
@@ -41,28 +40,19 @@ export default [
     },
     plugins: {
       "@typescript-eslint": tseslint,
-      react: reactPlugin,
       "react-hooks": reactHooksPlugin,
     },
     rules: {
       ...tseslint.configs.recommended.rules,
-      ...reactPlugin.configs.recommended.rules,
       ...reactHooksPlugin.configs.recommended.rules,
       "@typescript-eslint/no-explicit-any": "error",
       "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
       "@typescript-eslint/explicit-function-return-type": "off",
       "@typescript-eslint/no-floating-promises": "off", // Disable for MVP - too strict
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
       "no-console": ["warn", { allow: ["warn", "error"] }],
       "no-undef": "off", // TypeScript handles this
       "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-unused-expressions": "off",
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
     },
   },
 
